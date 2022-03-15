60ef534b5a1bb64933227fc00351bb59dfe49835
James <jemsgold21@gmail.com>
2022-03-15 00:24:47 -0500

    #trying to add pan + sauce gradients

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,26 @@
 document.querySelector('#mainOptions').classList.toggle('hidden') //default unhide
 
-let crust = document.getElementById('crust')
+//shorthand for getElementById
+let getElement = function(el) {
+    return document.getElementById(el)
+}
 
-class Crust {
-    constructor(width, height, background) { //num num string
+
+
+/* ------------- sauce --------------*/
+let sauce = document.getElementById('sauce')
+
+class Sauce {
+    constructor(width, height, sauceColor, border = 'none', gradientColor = sauceColor, gradientColor2 = sauceColor, gradientColor3 = gradientColor, gradientColor4 = sauceColor) { //num num string string string
         this.height = height;
         this.width = width;
-        this.background = background;
+        this.sauceColor = sauceColor;
+        this.border = border;
+        this.gradientColor = gradientColor;
+        this.gradientColor2 = gradientColor2;
+        this.gradientColor3 = gradientColor3;
+        this.gradientColor4 = gradientColor4;
+
     }
 
     get display() {
@@ -14,16 +28,98 @@ class Crust {
     }
 
     display() {
-        crust.style.background = this.background;
-        crust.style.width = `${this.width}px`;
-        crust.style.height = `${this.height}px`;
+        sauce.style = `background: radial-gradient(ellipse at center, ${this.sauceColor} 0%, ${this.gradientColor}, ${this.gradientColor2}, ${this.gradientColor3}, ${this.gradientColor4} 80%);`
+        //sauce.style.sauceColor = this.sauceColor;
+        sauce.style.width = `${this.width}px`;
+        sauce.style.height = `${this.height}px`;
+        sauce.style.zIndex = '200';
+        sauce.style.borderRadius = '50%';
+        sauce.style.border = `${this.border}`
     }
 }
 
-let basicCrust = new Crust(340, 340, 'tan')
+let basicSauce = new Sauce (310,2/3 * 310,'#c70e0e', '2px brown solid', '#881111', '#9e1919')
+basicSauce.display(); // display default
+
+let noSauce = new Sauce (0,0,'')
+let unicornSauce = new Sauce (310,2/3 * 310,'lightgray', '3px lightgray solid', '#9ea3a5', '#776f6f', '#878388', '#7ea3c5')
+let alfredoSauce = new Sauce (310, 2/3 * 310, '#fbfcf5', '2px #e0e0db solid', '#f3f3e0')
+
+
+let basicSauceOption = getElement('basicSauce')
+let noSauceOption = getElement('noSauce')
+let alfredoSauceOption = getElement('alfredoSauce')
+let unicornSauceOption = getElement('unicornSauce')
+
+basicSauceOption.addEventListener('click',function() {basicSauce.display()})
+noSauceOption.addEventListener('click',function() {noSauce.display()})
+alfredoSauceOption.addEventListener('click',function() {alfredoSauce.display()})
+unicornSauceOption.addEventListener('click',function() {unicornSauce.display()})
+
+
+/* -------- crust -----------*/
+
+let crust = document.getElementById('crust')
+let lightcrust = 'rgb(226, 214, 157)' //crustcolor
+
+class Crust {
+    constructor(width, height, background, isPan = false) { //num num string
+        this.height = height
+        this.width = width
+        this.oldHeight = 2/3 * this.width;
+        this.background = background
+        this.isPan = isPan
+    }
+
+    get display() {
+        console.log('test')
+        return this.display()
+    }
+
+    pan() {
+        if (this.isPan !== true) { //norm settings
+            crust.style.top = '0'
+            crust.style.bottom = '0'
+            crust.style.left = '0'
+            crust.style.right = '0'
+            crust.style.margin = 'auto'
+            crust.style.borderRadius = '50%';
+
+
+        } else {
+            crust.style.top = `calc(50% - ${this.oldHeight / 2}px)`
+            //crust.style.top = `100px`
+            crust.style.bottom = 'none'
+            crust.style.left = `calc(50% - ${this.width / 2}px)`
+            crust.style.right = 'none'
+            crust.style.margin = 'none'
+            crust.style.borderRadius = `50% / ${this.pan()}`;
+        }
+    }
+
+    display() {
+        crust.style.background = this.background;
+        crust.style.width = `${this.width}px`;
+        crust.style.height = `${this.height}px`;
+        this.pan();
+    }
+}
+
+let basicCrust = new Crust(340, 2/3 * 340, lightcrust)
 basicCrust.display();
 
 let noCrust = new Crust(0,0,'')
+let panCrust = new Crust(340, 2/3 * 340 + 40, lightcrust, true)
+
+let basicCrustOption = getElement('basicCrust')
+let noCrustOption = getElement('noCrust')
+let panCrustOption = getElement('panCrust')
+
+
+basicCrustOption.addEventListener('click',function() {basicCrust.display()})
+noCrustOption.addEventListener('click',function() {noCrust.display()})
+panCrustOption.addEventListener('click',function() {panCrust.display()})
+
 //noCrust.display()
 /*
 
@@ -37,6 +133,9 @@ function basicCrust() {
     crust.style.height = `${pSize}px`;
 }
 */
+
+
+
 
 //switch to the menu that is selected- grab html id from the triggering onclick
 function changeOption(evt) {
@@ -55,7 +154,9 @@ function changeOption(evt) {
         case 'goToToppings' :
             flipOn = 'toppingsOptions';
             break;
-        case 'goToOptions' :
+        case 'goToOptions1' :
+        case 'goToOptions2' :
+        case 'goToOptions3' :
             flipOn = 'mainOptions' ;
             break;
     }
@@ -63,9 +164,14 @@ function changeOption(evt) {
     document.getElementById(flipOn).classList.toggle('hidden') //unhide new element
 }
 
-let crustOptions = document.getElementById('goToCrust')
-let mainOptions = document.getElementById('goToOptions')
-
+let crustOptions = getElement('goToCrust')
+let sauceOptions = getElement('goToSauce')
+let mainOptions1 = getElement('goToOptions1')
+let mainOptions2 = getElement('goToOptions2')
+let mainOptions3 = getElement('goToOptions3')
 
 crustOptions.addEventListener('click',changeOption)
-mainOptions.addEventListener('click',changeOption)
+mainOptions1.addEventListener('click',changeOption)
+mainOptions2.addEventListener('click',changeOption)
+mainOptions3.addEventListener('click',changeOption)
+sauceOptions.addEventListener('click',changeOption)
